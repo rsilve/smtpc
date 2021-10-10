4f690d33b28596e97c75320612fda50b5fc8d015
Robert Silve <robert@silve.net>
2021-10-10 20:41:39 +0200

    add test

--- src/test/java/net/silve/smtpc/handler/SmtpClientFSEHandlerTest.java
+++ src/test/java/net/silve/smtpc/handler/SmtpClientFSEHandlerTest.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.MessageToMessageEncoder;
 import io.netty.handler.codec.smtp.*;
+import net.silve.smtpc.session.ListMessageFactory;
 import net.silve.smtpc.session.Message;
 import net.silve.smtpc.session.SmtpSession;
 import org.junit.jupiter.api.Test;
@@ -453,5 +454,83 @@ class SmtpClientFSEHandlerTest {
         assertFalse(channel.isActive());
 
     }
+
+    @Test
+    void shouldHandleBasicSessionMultiMessage() {
+        RecyclableSmtpContent content = RecyclableSmtpContent.newInstance(Unpooled.copiedBuffer("bb".getBytes(StandardCharsets.UTF_8)));
+        RecyclableSmtpContent content2 = RecyclableSmtpContent.newInstance(Unpooled.copiedBuffer("bb".getBytes(StandardCharsets.UTF_8)));
+        SmtpSession session = new SmtpSession("host", 25)
+                .setMessageFactory(
+                        new ListMessageFactory(new Message()
+                                .setSender("sender")
+                                .setRecipient("recipient")
+                                .setChunks(content, content2),
+                                new Message()
+                                .setSender("sender")
+                                .setRecipient("recipient")
+                                .setChunks(content, content2)
+                                )
+                )
+                .setExtendedHelo(false);
+        SmtpClientFSEHandler handler = new SmtpClientFSEHandler(session);
+        EmbeddedChannel channel = new EmbeddedChannel(handler);
+        channel.writeInbound(new DefaultSmtpResponse(220, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(354, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(354, "Ok"));
+        channel.writeInbound(new DefaultSmtpResponse(250, "Ok"));
+
+        channel.writeInbound(new DefaultSmtpResponse(221, "Ok"));
+        assertTrue(channel.finish());
+        SmtpRequest outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.HELO, outbound.command());
+        assertEquals("localhost", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.MAIL, outbound.command());
+        assertEquals("FROM:<sender>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.RCPT, outbound.command());
+        assertEquals("TO:<recipient>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.DATA, outbound.command());
+        SmtpContent c = channel.readOutbound();
+        assertEquals(content, c);
+        c = channel.readOutbound();
+        assertEquals(content.content(), c.content());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.RSET, outbound.command());
+        outbound = channel.readOutbound();
+
+        assertEquals(SmtpCommand.HELO, outbound.command());
+        assertEquals("localhost", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.MAIL, outbound.command());
+        assertEquals("FROM:<sender>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.RCPT, outbound.command());
+        assertEquals("TO:<recipient>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.DATA, outbound.command());
+        c = channel.readOutbound();
+        assertEquals(content, c);
+        c = channel.readOutbound();
+        assertEquals(content.content(), c.content());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.QUIT, outbound.command());
+        outbound = channel.readOutbound();
+        assertNull(outbound);
+        assertFalse(channel.isActive());
+
+    }
+
 
 }
