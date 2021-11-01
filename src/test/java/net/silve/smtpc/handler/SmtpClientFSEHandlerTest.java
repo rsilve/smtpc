e3ec69557e6d26228338eee660563b6a7b24b3ae
Robert Silve <robert@silve.net>
2021-11-01 08:01:28 +0100

    add test for coverage on tls

--- src/test/java/net/silve/smtpc/handler/SmtpClientFSEHandlerTest.java
+++ src/test/java/net/silve/smtpc/handler/SmtpClientFSEHandlerTest.java
@@ -535,4 +535,51 @@ class SmtpClientFSEHandlerTest {
     }
 
 
+    @Test
+    void shouldHandleBasicSessionWithTLS() throws SSLException {
+        RecyclableSmtpContent content = RecyclableSmtpContent.newInstance(Unpooled.copiedBuffer("bb".getBytes(StandardCharsets.UTF_8)));
+        RecyclableSmtpContent content2 = RecyclableSmtpContent.newInstance(Unpooled.copiedBuffer("bb".getBytes(StandardCharsets.UTF_8)));
+        SmtpSession session = SmtpSession.newInstance("host", 25)
+                .setMessageFactory(
+                        new Message()
+                                .setSender("sender")
+                                .setRecipient("recipient")
+                                .setChunks(content, content2)
+                                .factory()
+                )
+                .setExtendedHelo(false);
+        SmtpClientFSEHandler handler = new SmtpClientFSEHandler(session, new Config().useTls(true));
+        EmbeddedChannel channel = new EmbeddedChannel(handler);
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(220, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(250, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(250, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(250, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(354, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(250, "Ok")));
+        assertFalse(channel.writeInbound(new DefaultSmtpResponse(221, "Ok")));
+        assertTrue(channel.finish());
+        SmtpRequest outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.HELO, outbound.command());
+        assertEquals("localhost", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.MAIL, outbound.command());
+        assertEquals("FROM:<sender>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.RCPT, outbound.command());
+        assertEquals("TO:<recipient>", outbound.parameters().get(0).toString());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.DATA, outbound.command());
+        SmtpContent c = channel.readOutbound();
+        assertEquals(content, c);
+        c = channel.readOutbound();
+        assertEquals(content.content(), c.content());
+        outbound = channel.readOutbound();
+        assertEquals(SmtpCommand.QUIT, outbound.command());
+        outbound = channel.readOutbound();
+        assertNull(outbound);
+        assertFalse(channel.isActive());
+
+    }
+
+
 }
