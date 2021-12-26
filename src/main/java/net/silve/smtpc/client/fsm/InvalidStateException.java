47112567f5e57afe125dd4f1a9d0132ebab56c8f
Robert Silve <robert@silve.net>
2021-12-26 09:39:26 +0100

    fix sonar warn

--- src/main/java/net/silve/smtpc/client/fsm/InvalidStateException.java
+++ src/main/java/net/silve/smtpc/client/fsm/InvalidStateException.java
@@ -2,9 +2,10 @@ package net.silve.smtpc.client.fsm;
 
 import org.jetbrains.annotations.NotNull;
 
+
 public class InvalidStateException extends Exception {
 
-    private final State state;
+    private final transient State state;
 
     public InvalidStateException(@NotNull State state) {
         super();
