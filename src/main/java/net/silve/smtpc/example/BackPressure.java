5c5fd15ad4360492fe816c71d2689e1e519d47a7
Robert Silve <robert@silve.net>
2022-01-23 15:25:36 +0100

    refactor conncurrent example

--- src/main/java/net/silve/smtpc/example/BackPressure.java
+++ src/main/java/net/silve/smtpc/example/BackPressure.java
@@ -1,22 +1,9 @@
 package net.silve.smtpc.example;
 
-import io.netty.util.concurrent.DefaultEventExecutorGroup;
-import io.netty.util.concurrent.Promise;
 import net.silve.smtpc.SmtpClient;
-import net.silve.smtpc.SmtpContentBuilder;
 import net.silve.smtpc.client.SmtpClientConfig;
-import net.silve.smtpc.message.ListMessageFactory;
-import net.silve.smtpc.message.Message;
-import net.silve.smtpc.message.SmtpSession;
 
-import java.util.List;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -31,84 +18,22 @@ public class BackPressure {
     private static final String[] RECIPIENTS = IntStream.range(1, 5)
             .mapToObj(value -> String.format("devnull+%d@silve.net", value)).toArray(String[]::new);
     private static final boolean USE_PIPELINING = true;
-    private static final int NUMBER_OF_MESSAGES = 50000;
-    private static final int BATCH_SIZE = 100;
-    private static final int POOL_SIZE = 300;
-
-    private static final Logger logger = LoggerFactory.getInstance();
-    private static byte[] contentBytes;
-    private static BlockingDeque<Boolean> poolQueue;
-    private static final LogListener logListener = new LogListener();
-    private static long globalStartedAt;
-    private static final DefaultEventExecutorGroup executors = new DefaultEventExecutorGroup(2);
-
-    static {
-        try {
-            contentBytes = BackPressure.class.getResourceAsStream("/example/fixture002.eml").readAllBytes();
-        } catch (Exception e) {
-            LoggerFactory.getInstance().log(Level.SEVERE, "cannot read message", e);
-        }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        LoggerFactory.configure(Level.INFO);
-        initialize();
+    private static final int NUMBER_OF_MESSAGES = 100_000;
+    private static final int BATCH_SIZE = 10;
+    private static final int POOL_SIZE = 200;
 
 
-        SmtpClient client = new SmtpClient(
-                new SmtpClientConfig().setGreeting("greeting.tld").usePipelining(USE_PIPELINING));
-        AtomicInteger max = new AtomicInteger(NUMBER_OF_MESSAGES);
-        AtomicLong totalDuration = new AtomicLong(0L);
-
-
-        Promise<Void> completedPromise = executors.next().newPromise();
-        completedPromise.addListener(future -> {
-            long totalDurationNano = totalDuration.longValue();
-            long duration = System.nanoTime() - globalStartedAt;
-            long avgConcurrency = Math.max(1, totalDurationNano / duration);
-            long avgDuration = totalDurationNano / NUMBER_OF_MESSAGES;
-
-            long durationMS = duration / 1000000;
-            double rate = ((double) NUMBER_OF_MESSAGES * 1000 * 1000000) / duration;
-
-            logger.info(() -> String.format("!!! total_duration=%dms, avg=%dms, avg_rate=%.2fm/s, avg_concurrency=%d",
-                    durationMS, avgDuration / 1000000, rate, avgConcurrency));
-            client.shutdownGracefully();
-            executors.shutdownGracefully();
-        });
-
-        for (int i = 0; i < NUMBER_OF_MESSAGES; i += BATCH_SIZE) {
-            sendMessage(client, max, totalDuration, completedPromise);
-        }
-        LoggerFactory.getInstance().log(Level.INFO, "!!! All message posted");
-
-    }
-
-    private static void sendMessage(SmtpClient client, AtomicInteger max, AtomicLong totalDuration,
-            Promise<Void> promise) throws InterruptedException {
-        poolQueue.take();
-        List<Message> messages = IntStream.range(0, BATCH_SIZE)
-                .mapToObj(value -> new Message().setSender(SENDER).setRecipients(RECIPIENTS)
-                        .setChunks(SmtpContentBuilder.chunks(contentBytes).iterator()))
-                .collect(Collectors.toList());
-        final SmtpSession session = SmtpSession.newInstance(HOST, PORT);
-        session.setMessageFactory(new ListMessageFactory(messages)).setListener(logListener);
-        final long startAt = System.nanoTime();
-        client.run(session).addListener(future -> {
-            poolQueue.put(true);
-            long duration = System.nanoTime() - startAt;
-            totalDuration.addAndGet(duration);
-            int step = max.addAndGet(-BATCH_SIZE);
-            if (step <= 0) {
-                promise.setSuccess(null);
+    public static void main(String[] args) {
+        SmtpClientConfig config = new SmtpClientConfig().setGreeting("greeting.tld").usePipelining(USE_PIPELINING);
+        SmtpClient client = new SmtpClient(config);
+        ConcurrentRunner runner = new ConcurrentRunner(client, NUMBER_OF_MESSAGES, POOL_SIZE, BATCH_SIZE);
+        runner.execute(r -> {
+            for (int i = 0; i < r.getNumberOfMessage(); i += r.getBatchSize()) {
+                r.sendMessage(SENDER, RECIPIENTS, HOST, PORT);
             }
+            LoggerFactory.getInstance().log(Level.INFO, "!!! All message posted");
         });
     }
 
-    private static void initialize() {
-        poolQueue = new LinkedBlockingDeque<>(POOL_SIZE);
-        IntStream.range(0, POOL_SIZE).forEach(value -> poolQueue.add(true));
-        globalStartedAt = System.nanoTime();
 
-    }
 }
--- src/main/java/net/silve/smtpc/example/Concurrent.java
+++ src/main/java/net/silve/smtpc/example/Concurrent.java
@@ -1,19 +1,9 @@
 package net.silve.smtpc.example;
 
-import io.netty.util.concurrent.DefaultEventExecutorGroup;
-import io.netty.util.concurrent.Promise;
-import net.silve.smtpc.SmtpContentBuilder;
 import net.silve.smtpc.SmtpClient;
-import net.silve.smtpc.message.Message;
-import net.silve.smtpc.message.SmtpSession;
 import net.silve.smtpc.client.SmtpClientConfig;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
 /**
@@ -27,60 +17,20 @@ public class Concurrent {
     private static final String SENDER = "sender@domain.tld";
     private static final String[] RECIPIENTS = IntStream.range(1, 5).mapToObj(value -> String.format("devnull+%d@silve.net", value)).toArray(String[]::new);
     private static final boolean USE_PIPELINING = true;
-    private static final int NUMBER_OF_MESSAGES = 100;
+    private static final int NUMBER_OF_MESSAGES = 1000;
     private static final long DELAY_MILLIS = 50;
 
-    private static final Logger logger = LoggerFactory.getInstance();
 
-    public static void main(String[] args) throws IOException {
-        LoggerFactory.configure(Level.INFO);
+    public static void main(String[] args) {
 
-        byte[] contentBytes = Concurrent.class.getResourceAsStream("/example/fixture001.eml").readAllBytes();
-
-        DefaultEventExecutorGroup executors = new DefaultEventExecutorGroup(2);
-        SmtpClient client = new SmtpClient(new SmtpClientConfig().setGreeting("greeting.tld").usePipelining(USE_PIPELINING));
-        AtomicInteger max = new AtomicInteger(NUMBER_OF_MESSAGES);
-        AtomicLong totalDuration = new AtomicLong(0L);
-
-        final long globalStartedAt = System.nanoTime();
-        Promise<Void> promise = executors.next().newPromise();
-        promise.addListener(future -> {
-            long totalDurationNano = totalDuration.longValue();
-            long duration = System.nanoTime() - globalStartedAt;
-            long avgConcurrency = Math.max(1, totalDurationNano / duration);
-            long avgDuration = totalDurationNano / NUMBER_OF_MESSAGES;
-
-            long durationMS = duration / 1000000;
-            double rate = ((double) NUMBER_OF_MESSAGES * 1000 * 1000000) / duration;
-
-            logger.info(() -> String.format("!!! total_duration=%dms, avg=%dms, avg_rate=%.2fm/s, avg_concurrency=%d",
-                    durationMS, avgDuration / 1000000, rate, avgConcurrency));
-            client.shutdownGracefully();
-            executors.shutdownGracefully();
+        SmtpClientConfig config = new SmtpClientConfig().setGreeting("greeting.tld").usePipelining(USE_PIPELINING);
+        SmtpClient client = new SmtpClient(config);
+        ConcurrentRunner runner = new ConcurrentRunner(client, NUMBER_OF_MESSAGES, 50, 10);
+        runner.execute(r -> {
+            for (int i = 0; i < r.getNumberOfMessage(); i += r.getBatchSize()) {
+                r.getExecutor().schedule(() -> r.sendMessage(SENDER, RECIPIENTS, HOST, PORT),
+                        i * DELAY_MILLIS, TimeUnit.MILLISECONDS);
+            }
         });
-        LogListener logListener = new LogListener();
-        for (int i = 0; i < NUMBER_OF_MESSAGES; i++) {
-            executors.schedule(() -> {
-                final SmtpSession session = SmtpSession.newInstance(HOST, PORT);
-                session
-                        .setMessageFactory(
-                                new Message().setSender(SENDER)
-                                        .setRecipients(RECIPIENTS)
-                                        .setChunks(SmtpContentBuilder.chunks(contentBytes).iterator())
-                                        .factory())
-                        .setListener(logListener);
-                final long startAt = System.nanoTime();
-                client.run(session).addListener(future -> {
-                    long duration = System.nanoTime() - startAt;
-                    totalDuration.addAndGet(duration);
-                    int step = max.decrementAndGet();
-                    if (step <= 0) {
-                        promise.setSuccess(null);
-                    }
-                });
-            },
-                    i * DELAY_MILLIS, TimeUnit.MILLISECONDS);
-        }
-
     }
 }
