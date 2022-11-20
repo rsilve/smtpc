7d1ccbe51fbc7eca07fe04b2ce60830f9d6ee30f
Robert Silve <robert@silve.net>
2022-11-20 09:07:58 +0100

    feat: add capacity limit on internal queue (#50)
    * feat: add capacity limit on internal queue
    
    * feat: fix sonar warning
    
    * feat: fix coverage

--- src/test/java/net/silve/smtpc/message/QueueMessageFactoryTest.java
+++ src/test/java/net/silve/smtpc/message/QueueMessageFactoryTest.java
@@ -10,42 +10,93 @@ class QueueMessageFactoryTest {
     void shouldExists() {
         QueueMessageFactory factory = new QueueMessageFactory();
         assertNotNull(factory);
-        assertEquals(Integer.MAX_VALUE, factory.capacity());
-        assertEquals(10L, factory.timeoutMillis());
+        assertEquals(QueueMessageFactory.DEFAULT_CAPACITY, factory.capacity());
+        assertEquals(QueueMessageFactory.DEFAULT_TIMEOUT_MILLI, factory.timeoutMillis());
+        assertEquals(QueueMessageFactory.DEFAULT_LIMIT, factory.getLimit());
     }
 
     @Test
-    void shouldCompleteOnTimeout() {
+    void shouldHaveConstructorTimeout() {
+        QueueMessageFactory factory = new QueueMessageFactory(0L);
+        assertNotNull(factory);
+        assertEquals(QueueMessageFactory.DEFAULT_CAPACITY, factory.capacity());
+        assertEquals(0L, factory.timeoutMillis());
+        assertEquals(QueueMessageFactory.DEFAULT_LIMIT, factory.getLimit());
+    }
+
+    @Test
+    void shouldHaveConstructorLimit() {
+        QueueMessageFactory factory = new QueueMessageFactory(1);
+        assertNotNull(factory);
+        assertEquals(QueueMessageFactory.DEFAULT_CAPACITY, factory.capacity());
+        assertEquals(QueueMessageFactory.DEFAULT_TIMEOUT_MILLI, factory.timeoutMillis());
+        assertEquals(1, factory.getLimit());
+    }
+
+    @Test
+    void shouldHaveConstructorLimitAndTimeout() {
+        QueueMessageFactory factory = new QueueMessageFactory(0L, 1);
+        assertNotNull(factory);
+        assertEquals(QueueMessageFactory.DEFAULT_CAPACITY, factory.capacity());
+        assertEquals(0L, factory.timeoutMillis());
+        assertEquals(1, factory.getLimit());
+    }
+
+    @Test
+    void shouldHaveSetterForLimit() {
+        QueueMessageFactory factory = new QueueMessageFactory();
+        factory.setLimit(1);
+        assertEquals(1, factory.getLimit());
+    }
+
+    @Test
+    void shouldCompleteOnTimeout() throws InterruptedException {
         QueueMessageFactory factory = new QueueMessageFactory(0L);
         assertFalse(factory.isCompleted());
         factory.next();
         assertNull(factory.next());
         assertTrue(factory.isCompleted());
         assertNull(factory.next());
+        assertFalse(factory.offer(new Message()));
+        assertFalse(factory.offer(new Message(), 2L));
     }
 
     @Test
-    void shouldCompleteOnCapacity() {
-        QueueMessageFactory factory = new QueueMessageFactory(0);
+    void shouldCompleteOnLimt() throws InterruptedException {
+        QueueMessageFactory factory = new QueueMessageFactory(QueueMessageFactory.DEFAULT_TIMEOUT_MILLI, QueueMessageFactory.DEFAULT_CAPACITY, 0);
         assertTrue(factory.isCompleted());
-        assertFalse(factory.add(new Message()));
+        assertFalse(factory.put(new Message()));
         assertNull(factory.next());
     }
 
     @Test
-    void shouldReturnNextMessage() {
+    void shouldReturnNextMessage() throws InterruptedException {
         QueueMessageFactory factory = new QueueMessageFactory();
         Message message = new Message();
-        factory.add(message);
+        factory.put(message);
         assertEquals(message, factory.next());
     }
 
     @Test
-    void shouldReturnNullIfThereIsNoNextMessage() {
+    void shouldReturnNullIfThereIsNoNextMessage() throws InterruptedException {
         QueueMessageFactory factory = new QueueMessageFactory(1L);
         Message message = new Message();
-        factory.add(message);
+        factory.put(message);
         assertEquals(message, factory.next());
         assertNull(factory.next());
+    }
+
+    @Test
+    void shouldReturnFalseIfCapacityExceeded() {
+        QueueMessageFactory factory = new QueueMessageFactory(QueueMessageFactory.DEFAULT_TIMEOUT_MILLI, 1, QueueMessageFactory.DEFAULT_LIMIT);
+        assertTrue(factory.offer(new Message()));
+        assertFalse(factory.offer(new Message()));
+    }
+
+    @Test
+    void shouldWaitToOffer() throws InterruptedException {
+        QueueMessageFactory factory = new QueueMessageFactory(QueueMessageFactory.DEFAULT_TIMEOUT_MILLI, 1, QueueMessageFactory.DEFAULT_LIMIT);
+        factory.offer(new Message());
+        assertFalse(factory.offer(new Message(), 10L));
     }
 }
